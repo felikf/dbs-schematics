b88e2003dd34847e40b7cee816b8023674466712
Felix Fatka <felix.fatka@ext.csas.cz>
2019-06-17 22:48:38 +0200

    sections

--- src/dbs-module/files/__name@dasherize__/__name@dasherize__.component.ts
+++ src/dbs-module/files/__name@dasherize__/__name@dasherize__.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { Operations } from '@core/model/operation';
+import { DataSectionItem } from '@core/components/data-section/data-section.component';
 
 @Component({
   selector: 'dbs-<%= dasherize(name) %>',
@@ -7,12 +11,31 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 })
 export class <%= classify(name) %>Component implements OnInit, OnDestroy {
 
+  actionLinks$: Observable<Operations>;
+  itemsLeft: DataSectionItem[];
+  itemsRight: DataSectionItem[];
+
   constructor() {}
 
   toggleHistory(tabIndex: number): void {
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.configureSections({});
+  }
 
   ngOnDestroy(): void {}
+
+  private configureSections(data: any): void {
+    if (data) {
+      this.itemsLeft = [
+        { label: 'Left', value: 'Left Data' }
+      ];
+
+      this.itemsRight = [
+        { label: 'Right', value: 'Right Data' }
+      ];
+    }
+  }
+
 }
